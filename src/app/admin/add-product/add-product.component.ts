7e3e88dd4df17ab6e05bb6bdcf7eb14b9f2d663c
KelvinKoech <koechkelvin2001@gmail.com>
2023-03-01 12:50:16 +0300

    admin page

---
+++ src/app/admin/add-product/add-product.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { ApiService } from 'src/app/services/api.service';
+import { item } from 'src/app/services/cart.service';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-add-product',
+  templateUrl: './add-product.component.html',
+  styleUrls: ['./add-product.component.css']
+})
+
+export class AddProductComponent implements OnInit{
+    
+    form!: FormGroup
+    products!: item
+    Category: string[]=['--Select category--', 'electronics', 'jewelery',"men's clothing","women's clothing"]
+
+     constructor(private http:HttpClient, private apiService:ApiService, private fb:FormBuilder, private router:Router){}
+
+     ngOnInit(): void{
+        this.form= this.fb.group({
+            TITLE:[null,[Validators.required]],
+            DESCRIPTION:[null,[Validators.required]],
+            PRICE:[null,[Validators.required]],
+            CATEGORY:[null,[Validators.required]],
+            IMAGE:[null,[Validators.required]],
+            QUANTITY:[null,[Validators.required]]
+        })
+     }
+
+     addProduct(){
+        let obj={
+            form : this.form.value
+        };
+
+       this.apiService.addProduct(obj.form).subscribe(res=>{
+         console.log(res);
+         this.router.navigate(['home/allProducts'])
+       }) 
+     }
+     
+}
---
+++ src/app/admin/login/login.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/auth/auth.service';
+
+@Component({
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
+})
+
+export class LoginComponent implements OnInit {
+
+    constructor(private fb:FormBuilder, private auth:AuthService, private router:Router) { }
+    form!: FormGroup;
+
+    ngOnInit(): void {
+        this.form = this.fb.group({
+            email: [null, [Validators.required, Validators.email]],
+            password: [null, [Validators.required, Validators.minLength(8)]],
+        })
+    }
+
+    onLogin(){
+        console.log(this.form.value.email);
+        if(this.form.value.email == this.auth.login().email && this.form.value.password== this.auth.login().password){
+        this.router.navigate(['home/allProducts'])
+
+        localStorage.setItem('email',this.form.value.email)
+        localStorage.setItem('password',this.form.value.password)
+        }
+        else{
+        this.router.navigate(['home/login'])
+        }  
+    }
+    
+}
